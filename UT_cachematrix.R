a13c7b7c0eedbc6df4a950802e26b59da8935cd5
ambikasam <ambika.jan@gmail.com>
2014-05-22 20:27:19 +0530

    updating the cache functions and adding unit tests

---
+++ UT_cachematrix.R
@@ -0,0 +1,82 @@
+##############################################################################
+## Title    : Unit tests for Caching inverse of a matrix
+## Author   : Ambika
+## Date     : May-2014
+##############################################################################
+
+### removing all the variables in the environment/workspace ###
+rm(list = ls())
+
+### source the main R script ###
+source("cachematrix.R")
+
+### Test case - a general function ###
+testCase <- function(test,str,pcnt,tcnt){
+        #if ( identical(i1,i2) && identical(t1,t2) )  { rs = "PASS"; pcnt = pcnt + 1 }
+        rs <- "FAIL"
+        if ( test ) { rs = "PASS"; pcnt = pcnt + 1 }
+        print(paste(tcnt,". ",str," : ", rs))
+        tcnt = tcnt + 1
+        cnt <- c(pcnt, tcnt)
+}
+######################################
+
+###             BLOCK 1         ###
+testData <- matrix(stats::rnorm(90000), nrow=300, ncol=300)
+test <- makeCacheMatrix(testData)
+system.time(t1 <- cacheSolve(test)) ## first call; w/o cache
+system.time(t2 <- cacheSolve(test)) ## second call; with cache
+
+t <- testCase(TRUE,'If Functions throw no error',1,1)
+######################################
+
+###             BLOCK 2         ###
+n=4
+testData <- matrix(rnorm(n^2), nrow=n)
+test <- makeCacheMatrix(testData)
+system.time(t1 <- cacheSolve(test)) ## first call; w/o cache
+system.time(t2 <- cacheSolve(test)) ## second call; with cache
+i1 <- diag(n)
+i2 <- round(t1 %*% testData, 4)
+
+t <- testCase(identical(i1,i2) && identical(t1,t2),'Identify matrix cross check',t[1],t[2])
+######################################
+
+###             BLOCK 3         ###
+#install.packages("magic")
+library(magic)
+
+testData <- magic(5)
+A <- makeCacheMatrix(testData)
+A1 <- A$get()
+A2 <- A$getsolve()
+cacheSolve(A) ### first time; w/o cache
+A3 <- A$getsolve()
+A4 <- cacheSolve(A)
+
+## set the cacheResolve result as testData
+A$set(testData)
+B2 <- A$getsolve()
+B3 <- cacheSolve(A)
+
+A$set(B3)
+B4 <- A$getsolve()
+B5 <- cacheSolve(A)
+
+cacheSolve(A)
+
+t <- testCase(identical(A1,testData),'makeCacheMatrix$get : getData and testData are identical',t[1],t[2])
+t <- testCase(is.null(A2),'makeCacheMatrix$getsolve: NULL',t[1],t[2])
+t <- testCase(length(testData) == length(A3),'makeCacheMatrix$getsolve: Matrix exists and length is of testData',t[1],t[2])
+t <- testCase(identical(A4,A3),'makeCacheMatrix$getsolve: cacheSolve from cache result data is identical to getSolve',t[1],t[2])
+t <- testCase(is.null(B2),'makeCacheMatrix$getsolve: reset and check NULL',t[1],t[2])
+t <- testCase(is.null(B4),'makeCacheMatrix$getsolve: set the inverse as data and NULL check',t[1],t[2])
+t <- testCase(all.equal(testData,B5),'cacheSolve: reset the data to inverse of testData and compare the cacheSolve of the inverse data and test Data for equality',t[1],t[2])
+######################################
+
+pcnt = t[1]
+tcnt = t[2]
+print("")
+print(paste("Total test cases :",tcnt))
+print(paste("Total PASS % : ",((pcnt/tcnt)*100),'%'),sep = "")
+################################################################################################
--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,52 @@
-## Caching invertse of a matrix
-## Author: Ambika
-## Date: May-2014
+##############################################################################
+## Title    : Caching inverse of a matrix
+## Author   : Ambika
+## Date     : May-2014
+## Notes    :  caching the inverse of a matrix rather than computing it repeatedly
+##                We assume that the matrix supplied is always invertible 
+## Other equivalent functions :
+##            We can also use ginv() of library MASS pacakge (instead of solve()).
+##
+## Example  : testData <- matrix(stats::rnorm(90000), nrow=300, ncol=300)
+##              test <- makeCacheMatrix(testData)
+##              system.time(t <- cacheSolve(test)) ## first call; w/o cache
+##              system.time(t <- cacheSolve(test)) ## second call; with cache
+##              t %*% testData
+##############################################################################
 
 ## Creating a cache matrix
-
+## This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)   
 }
 
 
 ## Get the cached inverse matrix; else set the inverse matrix in the cache
-
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
+## If the inverse has already been calculated (and the matrix has not changed), 
+##    then cacheinverse should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m            
 }
+
+##############################################################################
